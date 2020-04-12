d3a207e3375444829f2cc457c68d6c019a181b8f
hbgit <herberthb12@gmail.com>
2020-04-12 20:52:55 +0000

    Deploying to gh-pages from master @ 4035d13cc35299d1096374f606d68e6268ef2378 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,11 +7,15 @@ const RESOURCES = {
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/AssetManifest.json": "5ce0d8c7d05f64d826e0534f82afe127",
+"/assets/AssetManifest.json": "62d28eb7ed589961ce2b4d84291b1dba",
 "/assets/LICENSE": "832c683862cb4bf3929f34d31416f01b",
-"/assets/images/logo_gasvsalc.png": "9f9d18188ceb1e6300103640abb69fd1",
+"/assets/images/v5_img/logo.png": "1a4e05af51006dcadefbaa65872bcfc6",
+"/assets/images/v5_img/moeda_coroa.png": "c3e301a258a8f9b3a3e09549cca4b735",
+"/assets/images/v5_img/botao_jogar.png": "0faa94c13b40da8688108306601e1ea7",
+"/assets/images/v5_img/moeda_cara.png": "f6b337d27683ac1157d8e0b07401e9a2",
+"/assets/images/v5_img/botao_voltar.png": "d226fb50a5f6ef9ce7bcf8663bd27f74",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"/main.dart.js": "6291c606634d77f78abad2ecb1460d01",
+"/main.dart.js": "8adeadf650bcedf644538ee81ac5ded0",
 "/manifest.json": "267450d2e27a233810c283b77314b646"
 };
 
