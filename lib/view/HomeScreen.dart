8264fb8fb22571b426acb8c1edd28e8669b458e6
Herbert Rocha <herberthb12@gmail.com>
2020-04-16 16:02:47 -0400

    Home Screen
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- lib/view/HomeScreen.dart
+++ lib/view/HomeScreen.dart
@@ -8,10 +8,66 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  //Screen Actions
+
+
+  //Screen
   @override
   Widget build(BuildContext context) {
-    return Container(
-       
+    return Scaffold(
+      appBar: (
+        AppBar(
+          iconTheme: IconThemeData(
+            color: Colors.grey
+          ),
+          backgroundColor: Colors.white,
+          title: Image.asset(
+            "images/v7_img/youtube.png",
+            width: 98,
+            height: 22,
+          ),
+          actions: <Widget>[
+            // search
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: (){
+                print("search icon");
+              },
+            ),
+          ],
+        )
+      ),      
+      body: (
+        Container(
+          padding: EdgeInsets.all(15),
+          // Move screen
+        )
+      ),      
+      bottomNavigationBar: (
+        BottomNavigationBar(
+          currentIndex: 0,
+          //onTap: (){},
+          type: BottomNavigationBarType.shifting,
+          items: [
+              BottomNavigationBarItem(
+                backgroundColor: Colors.cyan,
+                icon: Icon(Icons.home)
+              ),
+              BottomNavigationBarItem(
+                backgroundColor: Colors.red,
+                icon: Icon(Icons.whatshot)
+              ),
+              BottomNavigationBarItem(
+                backgroundColor: Colors.orange,
+                icon:Icon(Icons.subscriptions)
+              ),
+              BottomNavigationBarItem(
+                backgroundColor: Colors.green,
+                icon: Icon(Icons.folder)
+              ),
+          ],
+        )
+      ),      
     );
   }
 }
