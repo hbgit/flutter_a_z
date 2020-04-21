5b2e06e3d6af495f514cb7a1bff8c2915cc07081
Herbert Rocha <herberthb12@gmail.com>
2020-04-21 10:57:11 -0400

    Minor changes
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -8,79 +8,81 @@ import 'package:test/test.dart';
 void main() {
   test("Checking video model", () {
     String jsonString = """ 
-  "items": [   
   {
-   "kind": "youtube#video",
-   "etag": "nxOHAKTVB7baOKsQgTtJIyGxcs8/PqmK3dQl2D1Z38JM7ai1MnWM68o",
-   "id": "7Y8g0BTXRh4",
-   "snippet": {
-    "publishedAt": "2020-04-20T14:25:35.000Z",
-    "channelId": "UCKy1dAqELo0zrOtPkf0eTMw",
-    "title": "The Mandalorian Season 2 Trailer | Disney+",
-    "description": "Din Djarin has traveled far, made many enemies, and shouldered the burden of some very precious cargo...but his journey is far from over. The Mandalorian is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy far from the authority of the New Republic.",
-    "thumbnails": {
-     "default": {
-      "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/default.jpg",
-      "width": 120,
-      "height": 90
-     },
-     "medium": {
-      "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/mqdefault.jpg",
-      "width": 320,
-      "height": 180
-     },
-     "high": {
-      "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/hqdefault.jpg",
-      "width": 480,
-      "height": 360
-     },
-     "standard": {
-      "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/sddefault.jpg",
-      "width": 640,
-      "height": 480
-     },
-     "maxres": {
-      "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/maxresdefault.jpg",
-      "width": 1280,
-      "height": 720
-     }
-    },
-    "channelTitle": "IGN",
-    "tags": [
-     "IGN",
-     "Mando",
-     "shows",
-     "Disney",
-     "Teaser",
-     "Sci-Fi",
-     "Trailer",
-     "Official",
-     "Disney +",
-     "Season 2",
-     "Mandalorian",
-     "Baby Yoda",
-     "The Child",
-     "Star Wars",
-     "Moff Gideon",
-     "Disney Plus",
-     "Star Wars: The Mandalorian",
-     "Disney+",
-     "mandalorian season 2",
-     "mandalorian theme",
-     "mandalorian trailer",
-     "mandalorian season 2 teaser",
-     "mandalorian season 2 trailer",
-     "mandalorian season 2 official trailer"
-    ],
-    "categoryId": "24",
-    "liveBroadcastContent": "none",
-    "localized": {
-     "title": "The Mandalorian Season 2 Trailer | Disney+",
-     "description": "Din Djarin has traveled far, made many enemies, and shouldered the burden of some very precious cargo...but his journey is far from over. The Mandalorian is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy far from the authority of the New Republic."
+    "items": [   
+    {
+    "kind": "youtube#video",
+    "etag": "nxOHAKTVB7baOKsQgTtJIyGxcs8/PqmK3dQl2D1Z38JM7ai1MnWM68o",
+    "id": "7Y8g0BTXRh4",
+    "snippet": {
+      "publishedAt": "2020-04-20T14:25:35.000Z",
+      "channelId": "UCKy1dAqELo0zrOtPkf0eTMw",
+      "title": "The Mandalorian Season 2 Trailer | Disney+",
+      "description": "Din Djarin has traveled far, made many enemies, and shouldered the burden of some very precious cargo...but his journey is far from over. The Mandalorian is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy far from the authority of the New Republic.",
+      "thumbnails": {
+      "default": {
+        "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/default.jpg",
+        "width": 120,
+        "height": 90
+      },
+      "medium": {
+        "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/mqdefault.jpg",
+        "width": 320,
+        "height": 180
+      },
+      "high": {
+        "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/hqdefault.jpg",
+        "width": 480,
+        "height": 360
+      },
+      "standard": {
+        "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/sddefault.jpg",
+        "width": 640,
+        "height": 480
+      },
+      "maxres": {
+        "url": "https://i.ytimg.com/vi/7Y8g0BTXRh4/maxresdefault.jpg",
+        "width": 1280,
+        "height": 720
+      }
+      },
+      "channelTitle": "IGN",
+      "tags": [
+      "IGN",
+      "Mando",
+      "shows",
+      "Disney",
+      "Teaser",
+      "Sci-Fi",
+      "Trailer",
+      "Official",
+      "Disney +",
+      "Season 2",
+      "Mandalorian",
+      "Baby Yoda",
+      "The Child",
+      "Star Wars",
+      "Moff Gideon",
+      "Disney Plus",
+      "Star Wars: The Mandalorian",
+      "Disney+",
+      "mandalorian season 2",
+      "mandalorian theme",
+      "mandalorian trailer",
+      "mandalorian season 2 teaser",
+      "mandalorian season 2 trailer",
+      "mandalorian season 2 official trailer"
+      ],
+      "categoryId": "24",
+      "liveBroadcastContent": "none",
+      "localized": {
+      "title": "The Mandalorian Season 2 Trailer | Disney+",
+      "description": "Din Djarin has traveled far, made many enemies, and shouldered the burden of some very precious cargo...but his journey is far from over. The Mandalorian is set after the fall of the Empire and before the emergence of the First Order. We follow the travails of a lone gunfighter in the outer reaches of the galaxy far from the authority of the New Republic."
+      }
     }
-   }
+    }
+    ]
   }
-  ]
 """;
 
     Map<String, dynamic> youtubeReturn = jsonDecode(jsonString);
