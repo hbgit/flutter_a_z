156afb1e1586fecb810fd2e3139375f950ab1e7b
Herbert Rocha <herberthb12@gmail.com>
2020-04-22 22:20:35 -0400

    App works
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- lib/model/Task.dart
+++ lib/model/Task.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Task {
 
   String id;  
@@ -11,6 +13,41 @@ class Task {
       this.status
     }
   );
+
+  List getListTaskFromJsonString(String jsonString){
+
+    List tasksData = json.decode(jsonString);
+    List tasksObj = [];
+
+    tasksData.forEach((element) {
+      Task tmpTask = Task(
+        id: element["id"],
+        description: element["description"],
+        status: element["status"],
+      );
+
+      tasksObj.add(tmpTask);
+
+    });
+
+    return tasksObj;
+
+  }
+
+  List getMapListTask (List task){
+    List mapTask = [];
+    Map<String, dynamic> tmpTask = Map();
+    
+    task.forEach((element) {
+      tmpTask["id"] = element.id;
+      tmpTask["description"] = element.description;
+      tmpTask["status"] = element.status;
+
+      mapTask.add(tmpTask);
+    });
+
+    return mapTask;    
+  }
 
    /*
    factory Task.fromJson(Map<String, dynamic> json){
