060e5c04488321da32067f4c33460f11b715333c
Herbert Rocha <herberthb12@gmail.com>
2020-04-27 14:27:52 -0400

    Add unit test
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,16 +1,39 @@
 // Import the test package and Counter class
+import 'package:flutter_a_z/model/Task.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
 
-  test("Checking Api call", () async {
-    /*
-    Api t1 = Api();
-    await t1.search("").then((value) {
-      expect(value.length, isNonZero);
-    });    
-    */
+  test("Checking task model", () async {
+    
+    Task task = Task(
+      id: "12q",
+      description: "Read paper",
+      priority: 7.0,
+      status: false
+    );
 
+    String jsonString = """
+    {
+      "id": "123abc",
+      "description": "Read paper",
+      "priority": 7.0,
+      "status": false
+    }
+    """;
+
+    List resultJson = task.getListTaskFromJsonString(jsonString);
+    expect(resultJson.length, isNonZero);
+
+    List getMapList = task.getMapListTask(resultJson);
+
+    getMapList.forEach((element) {
+      expect(element.id, "123abc");
+      expect(element.description, "Read paper");
+      expect(element.priority, 7.0);
+      expect(element.priority, false);
+    });    
+    
   });  
 
 }
