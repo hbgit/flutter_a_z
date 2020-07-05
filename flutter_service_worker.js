2ac718ad6bbf2dd363a264b76c8ff92f5ecac636
hbgit <herberthb12@gmail.com>
2020-07-05 00:27:28 +0000

    Deploying to gh-pages from master @ ac3c9bc6c52a761e185b38ca0ca8723e1008a741 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,22 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "49dcfc76529621c809583ac4a7357176",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"assets/LICENSE": "4732249f3e829a42829345b28b2a6ccc",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+  "main.dart.js": "98a8e246cf3391a9047685fe5b0dbfdf",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "267450d2e27a233810c283b77314b646",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/assets/v11/defaultUser.png": "7d61448ff9aacb86e27323ec612b9e9f",
+"assets/assets/v11/logo.png": "6acbfc9a5f176ae66db826d5182c5bc5",
+"assets/assets/v11/background.jpg": "cbdbadac0968f89a26e059816d406336",
+"assets/assets/v11/usuario.png": "1e2751a1f7bd7a155b8119a9806828f8",
+"assets/AssetManifest.json": "3860e5c4b45114e27b95dba469d01e4e",
+"assets/LICENSE": "098563bd2220c70512552fd053ba27db",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"index.html": "9fbcc4aef28a6e07e93351f63b10e120",
-"/": "9fbcc4aef28a6e07e93351f63b10e120"
+"index.html": "9cf2d24eb0be6ba9da88690278db0bef",
+"/": "9cf2d24eb0be6ba9da88690278db0bef"
 };
 
 self.addEventListener('activate', function (event) {
